826d273bb64ccfc6ba14a8a0fdf7fb818887f7a5
AaryaGondikar <aarya.gondikar241@vit.edu>
2025-08-10 20:38:41 +0530

    third commit

--- hellopython.py
+++ hellopython.py
@@ -1,1 +1,17 @@
 print('hello aarya')
+
+a=10
+b=20
+c=a+b
+print(c)
+print('hello world')
+
+d = a*b
+print(d)
+
+print('hello python')
+
+# To create a new branch using git, run the following command in your terminal:
+# git checkout -b <branch-name>
+# Example:
+# git checkout -b my-new-branch
